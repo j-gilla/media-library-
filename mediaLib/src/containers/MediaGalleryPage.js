c227a3ea01399c7a05eb15f75724ecf56ca9fff3
gilbertjacke <gilbertjacke@gmail.com>
2017-01-11 21:12:55 +0000

    media gallery page added

---
+++ mediaLib/src/containers/MediaGalleryPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { flickrImages, shutterStockVideos } from '../Api/api';
+
+class MediaGalleryPage extends Component {
+  componentDidMount() {
+    flickrImages('rain').then(images => console.log(images, 'Images'));
+    shutterStockVideos('rain').then(videos => console.log(videos, 'Videos'));
+  }
+
+  render() {
+    //Todo: Render videos & images here
+    return (<div></div>)
+  }
+}
+
+export default MediaGalleryPage;
