03a5ac803d22c3229e1d7d51c07e237531522a1c
gilbertjacke <gilbertjacke@gmail.com>
2017-01-12 22:02:39 +0000

    introduced sagas

---
+++ mediaLib/src/sagas/watcher.js
@@ -0,0 +1,9 @@
+import { takeLatest } from 'redux-saga';
+import { searchMediaSaga } from './mediaSaga';
+import * as types from '../constants/actionTypes';
+
+//Watches for SEARCH_MEDIA_REQUEST action types asynchronously
+
+export default function * watchSearchMedia() {
+  yeild* takeLatest(types.SEARCH_MEDIA_REQUEST, searchMediaSaga);
+}
